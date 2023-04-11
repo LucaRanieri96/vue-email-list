1cf19c1a29b7eb1805a4b9acd270343856834967
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-11 14:10:31 +0200

    Init html, css, vue.js, axios

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "Hello",
+      emailUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
+    };
+  },
+  methods: {
+    
+  },
+  mounted() {
+    
+  },
+}).mount("#app");
+
+
