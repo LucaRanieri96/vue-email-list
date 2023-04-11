4f5da07f111b5cd81ad317969fafa457b697be54
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-11 14:28:18 +0200

    Add function getTenEmails

--- assets/app.js
+++ assets/app.js
@@ -5,14 +5,26 @@ createApp({
   data() {
     return {
       message: "Hello",
-      emailUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
+      emailApi: "https://flynn.boolean.careers/exercises/api/random/mail",
+      emailList: [],
     };
   },
   methods: {
+    getTenEmails(){
+      for(let i = 0; i < 10; i++) {
+        axios
+        .get(this.emailApi)
+        .then((result)=> {
+            this.email = result.data.response;
+            this.emailList.push(this.email);
+        });
+      }
+    },
     
   },
   mounted() {
-    
+    this.getTenEmails();
+    console.log(this.emailList);
   },
 }).mount("#app");
 
